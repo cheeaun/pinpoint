f4f16474388b6ed6762a0862344c5c158f6b2b28
Lim Chee Aun <cheeaun@gmail.com>
2011-07-26 23:31:57 +0800

    Use Element.scrollIntoView

--- cssfrag.js
+++ cssfrag.js
@@ -1,27 +1,11 @@
-function offsetXY(elem)
-{
-	var x = y = 0;
-	if (elem.offsetParent)
-	{
-		do {
-			x += elem.offsetLeft;
-			y += elem.offsetTop;
-		} while (elem = elem.offsetParent);
-	};
-	return {'x':x,'y':y};
-};
-
-function CSSFrag() // does not support fallbacks
-{
-	var m;
-	if (m = decodeURIComponent(window.location.hash).match(/css\((.+)\)/))
-	{
-		var selector = m[1];
-		var elem = document.querySelector(selector);
-		var offset = offsetXY(elem);
-		window.scrollTo(offset.x, offset.y);
-		elem.focus();
-	};
+function CSSFrag(){
+	var m = decodeURIComponent(window.location.hash).match(/css\((.+)\)/);
+	if (!m) return;
+	var selector = m[1];
+	var elem = document.querySelector(selector);
+	if (!elem) return;
+	elem.scrollIntoView();
+	elem.focus();
 };
 
 // window.addEventListener('load', CSSFrag, false);
